bc8253f74b5e1ffae3812e3e1597a8b52d07ae6f
gilmarsouza <gilmar.limasou@gmail.com>
2017-02-22 22:59:53 -0300

    Capitulo 1 - Instalando o Entityframework e criando um pequeno banco

---
+++ lojaComEntity/Program.cs
@@ -0,0 +1,30 @@
+﻿using lojaComEntity.Entidades;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lojaComEntity
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var contexto = new EntidadesContext();
+
+            var gilmar = new Usuario()
+            {
+                Nome = "Gilmar",
+                Senha = "123"
+            };
+
+            contexto.Usuarios.Add(gilmar);
+            contexto.SaveChanges();
+            contexto.Dispose();
+
+            Console.WriteLine("Salvou o usuário");
+            Console.ReadLine();
+        }
+    }
+}
